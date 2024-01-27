58ebd59d194f9e22e8db9771ed1903a695d62da1
V <Vdragon.b@gmail.com>
2024-01-28 00:02:06 +0100

    rewrote a lot of stuffs, now fighting with LD scripts, which make even less sense than dts and are fully unreadable by humans

--- modules/hal_bouffalolab/bl602.cmake
+++ modules/hal_bouffalolab/bl602.cmake
@@ -4,7 +4,7 @@ set(bflb_soc_path_prefix ${ZEPHYR_HAL_BOUFFALOLAB_MODULE_DIR}/drivers/soc/${CONF
 
 zephyr_library_sources(
 ${bflb_soc_path_prefix}/startup/start.S
-${bflb_soc_path_prefix}/startup/start_load.c
+#${bflb_soc_path_prefix}/startup/start_load.c
 ${bflb_soc_path_prefix}/startup/system_bl602.c
 ${bflb_soc_path_prefix}/startup/interrupt.c
 )
@@ -23,6 +23,7 @@ ${bflb_soc_path_prefix}/src/bl602_ef_cfg.c
 ${bflb_soc_path_prefix}/src/bl602_glb.c
 ${bflb_soc_path_prefix}/src/bl602_hbn.c
 ${bflb_soc_path_prefix}/src/bl602_l1c.c
+${bflb_soc_path_prefix}/src/bl602_pds.c
 ${bflb_soc_path_prefix}/src/bl602_tzc_sec.c
 
 ${bflb_soc_path_prefix}/src/bl602_sflash_ext.c
@@ -44,3 +45,25 @@ SET(MARCH "rv32imafc")
 SET(MABI "ilp32f")
 
 zephyr_compile_definitions(CONFIG_IRQ_NUM=80)
+
+
+# bsp section
+
+zephyr_library_sources(
+${ZEPHYR_HAL_BOUFFALOLAB_MODULE_DIR}/bsp/board/${CONFIG_SOC_SUB_SERIES}dk/board.c
+${ZEPHYR_HAL_BOUFFALOLAB_MODULE_DIR}/bsp/board/${CONFIG_SOC_SUB_SERIES}dk/fw_header.c
+
+${ZEPHYR_HAL_BOUFFALOLAB_MODULE_DIR}/components/mm/mem.c
+${ZEPHYR_HAL_BOUFFALOLAB_MODULE_DIR}/components/mm/tlsf/tlsf.c
+${ZEPHYR_HAL_BOUFFALOLAB_MODULE_DIR}/components/mm/tlsf/bflb_tlsf.c
+)
+
+zephyr_include_directories(
+${ZEPHYR_HAL_BOUFFALOLAB_MODULE_DIR}/bsp/board/${CONFIG_SOC_SUB_SERIES}dk/
+
+${ZEPHYR_HAL_BOUFFALOLAB_MODULE_DIR}/components/mm/
+)
+
+zephyr_compile_definitions(
+CONFIG_TLSF
+)
