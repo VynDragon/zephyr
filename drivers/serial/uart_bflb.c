72e4af51b5155cec5193e04ee61d92c8248d2ce2
Camille BAUD <mail@massdriver.space>
2024-05-28 16:55:40 +0200

    Working interrupt based UART driver
    Signed-off-by: Camille BAUD <mail@massdriver.space>

--- drivers/serial/uart_bflb.c
+++ drivers/serial/uart_bflb.c
@@ -14,6 +14,7 @@
 #include <zephyr/drivers/pinctrl.h>
 #include <zephyr/arch/common/sys_io.h>
 #include <zephyr/drivers/timer/system_timer.h>
+#include <zephyr/irq.h>
 
 
 #include <soc.h>
@@ -34,8 +35,17 @@ struct bflb_config {
 	uint8_t tx_fifo_threshold;
 	uint8_t rx_fifo_threshold;
 	uint32_t base_reg;
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	uart_irq_config_func_t irq_config_func;
+#endif /* CONFIG_UART_INTERRUPT_DRIVEN */
 };
 
+struct bflb_data {
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	uart_irq_callback_user_data_t user_cb;
+	void *user_data;
+#endif /* CONFIG_UART_INTERRUPT_DRIVEN */
+};
 
 static void ITCMF uart_bflb_enabled(const struct device *dev, uint32_t enable)
 {
@@ -167,6 +177,201 @@ static uint32_t ITCMF uart_bflb_get_clock(void)
 	return 0;
 }
 
+
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+
+static int ITCMF uart_bflb_err_check(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t status = 0;
+	uint32_t tmpVal = 0;
+	int errors = 0;
+
+	status = sys_read32(cfg->base_reg + UART_INT_STS_OFFSET);
+	tmpVal = sys_read32(cfg->base_reg + UART_INT_CLEAR_OFFSET);
+
+	if (status & UART_URX_FER_INT) {
+		errors |= UART_ERROR_OVERRUN;
+	}
+
+	if (status & UART_UTX_FER_INT) {
+		errors |= UART_ERROR_OVERRUN;
+	}
+
+	if (status & UART_URX_PCE_INT) {
+		tmpVal |= UART_CR_URX_PCE_CLR;
+
+		errors |= UART_ERROR_PARITY;
+	}
+
+	sys_write32(tmpVal, cfg->base_reg + UART_INT_CLEAR_OFFSET);
+
+	return errors;
+}
+
+int ITCMF uart_bflb_irq_tx_ready(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_FIFO_CONFIG_1_OFFSET);
+	return (tmpVal & UART_TX_FIFO_CNT_MASK) > 0;
+}
+
+int ITCMF uart_bflb_irq_rx_ready(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_FIFO_CONFIG_1_OFFSET);
+	return (tmpVal & UART_RX_FIFO_CNT_MASK) > 0;
+}
+
+int ITCMF uart_bflb_fifo_fill(const struct device *dev, const uint8_t *tx_data, int len)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint8_t num_tx = 0U;
+
+	while (num_tx < len && uart_bflb_irq_tx_ready(dev) > 0) {
+		sys_write32( tx_data[num_tx++], cfg->base_reg + UART_FIFO_WDATA_OFFSET);
+	}
+
+	return num_tx;
+}
+
+int ITCMF uart_bflb_fifo_read(const struct device *dev, uint8_t *rx_data, const int size)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint8_t num_rx = 0U;
+
+	while ((num_rx < size) && uart_bflb_irq_rx_ready(dev) > 0) {
+		rx_data[num_rx++] = sys_read32(cfg->base_reg + UART_FIFO_RDATA_OFFSET);
+	}
+
+	return num_rx;
+}
+
+void ITCMF uart_bflb_irq_tx_enable(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_INT_MASK_OFFSET);
+	tmpVal = tmpVal & ~UART_CR_UTX_FIFO_MASK;
+	sys_write32(tmpVal, cfg->base_reg + UART_INT_MASK_OFFSET);
+}
+
+void ITCMF uart_bflb_irq_tx_disable(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_INT_MASK_OFFSET);
+	tmpVal = tmpVal | UART_CR_UTX_FIFO_MASK;
+	sys_write32(tmpVal, cfg->base_reg + UART_INT_MASK_OFFSET);
+}
+
+
+int ITCMF uart_bflb_irq_tx_complete(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+	int rc = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_STATUS_OFFSET);
+	rc = tmpVal & UART_STS_UTX_BUS_BUSY;
+	tmpVal = sys_read32(cfg->base_reg + UART_FIFO_CONFIG_1_OFFSET);
+	rc += tmpVal & UART_TX_FIFO_CNT_MASK;
+	return (rc > 0 ? 1 : 0);
+}
+
+void ITCMF uart_bflb_irq_rx_enable(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_INT_MASK_OFFSET);
+	tmpVal = tmpVal & ~UART_CR_URX_FIFO_MASK;
+	sys_write32(tmpVal, cfg->base_reg + UART_INT_MASK_OFFSET);
+}
+
+void ITCMF uart_bflb_irq_rx_disable(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_INT_MASK_OFFSET);
+	tmpVal = tmpVal | UART_CR_URX_FIFO_MASK;
+	sys_write32(tmpVal, cfg->base_reg + UART_INT_MASK_OFFSET);
+}
+
+void ITCMF uart_bflb_irq_err_enable(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_INT_MASK_OFFSET);
+	tmpVal = tmpVal & ~UART_CR_URX_PCE_MASK;
+	tmpVal = tmpVal & ~UART_CR_UTX_FER_MASK;
+	tmpVal = tmpVal & ~UART_CR_URX_FER_MASK;
+	sys_write32(tmpVal, cfg->base_reg + UART_INT_MASK_OFFSET);
+}
+
+void ITCMF uart_bflb_irq_err_disable(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	tmpVal = sys_read32(cfg->base_reg + UART_INT_MASK_OFFSET);
+	tmpVal = tmpVal | UART_CR_URX_PCE_MASK;
+	tmpVal = tmpVal | UART_CR_UTX_FER_MASK;
+	tmpVal = tmpVal | UART_CR_URX_FER_MASK;
+	sys_write32(tmpVal, cfg->base_reg + UART_INT_MASK_OFFSET);
+}
+
+int ITCMF uart_bflb_irq_is_pending(const struct device *dev)
+{
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = sys_read32(cfg->base_reg + UART_INT_STS_OFFSET);
+	uint32_t maskVal = sys_read32(cfg->base_reg + UART_INT_MASK_OFFSET);
+
+	/* only first 8 bits are not reserved */
+	return (((tmpVal & ~maskVal) & 0xFF) != 0 ? 1 : 0);
+}
+
+int ITCMF uart_bflb_irq_update(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	return 1;
+}
+
+void ITCMF uart_bflb_irq_callback_set(const struct device *dev,
+			      uart_irq_callback_user_data_t cb,
+			      void *user_data)
+{
+	struct bflb_data *const data = dev->data;
+
+	data->user_cb = cb;
+	data->user_data = user_data;
+}
+
+static void ITCMF uart_bflb_isr(const struct device *dev)
+{
+	struct bflb_data *const data = dev->data;
+	const struct bflb_config *const cfg = dev->config;
+	uint32_t tmpVal = 0;
+
+	if (data->user_cb) {
+		data->user_cb(dev, data->user_data);
+	}
+	/* clear interrupts that require ack*/
+	tmpVal = sys_read32(cfg->base_reg + UART_INT_CLEAR_OFFSET);
+	tmpVal = tmpVal | UART_CR_URX_RTO_CLR;
+	sys_write32(tmpVal, cfg->base_reg + UART_INT_CLEAR_OFFSET);
+}
+#endif /* CONFIG_UART_INTERRUPT_DRIVEN */
+
 static int ITCMF uart_bflb_configure(const struct device *dev)
 {
 	const struct bflb_config *cfg = dev->config;
@@ -230,8 +435,8 @@ cfg->base_reg + UART_BIT_PRD_OFFSET);
 		tx_cfg &= ~UART_CR_UTX_CTS_EN;
 	}
 
+	/* disable de-glitch function */
 	rx_cfg &= ~UART_CR_URX_DEG_EN;
-
 
 	/* Write config */
 	sys_write32(tx_cfg, cfg->base_reg + UART_UTX_CONFIG_OFFSET);
@@ -276,20 +481,32 @@ cfg->base_reg + UART_BIT_PRD_OFFSET);
 	tmpVal &= ~UART_DMA_RX_EN;
 	sys_write32(tmpVal, cfg->base_reg + UART_FIFO_CONFIG_0_OFFSET);
 
-	/* enable all irqs */
-	sys_write32(0xFFFFFFFF, cfg->base_reg + UART_INT_MASK_OFFSET);
-
-	uart_bflb_enabled(dev, 1);
-
 	return 0;
 }
 
 static int ITCMF uart_bflb_init(const struct device *dev)
 {
 	const struct bflb_config *cfg = dev->config;
+	int rc = 0;
 
 	pinctrl_apply_state(cfg->pincfg, PINCTRL_STATE_DEFAULT);
-	return uart_bflb_configure(dev);
+	rc = uart_bflb_configure(dev);
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	/* disable all irqs */
+	sys_write32(0x0, cfg->base_reg + UART_INT_EN_OFFSET);
+	/* clear all IRQS */
+	sys_write32(0xFF, cfg->base_reg + UART_INT_CLEAR_OFFSET);
+	/* mask all IRQs */
+	sys_write32(0xFFFFFFFF, cfg->base_reg + UART_INT_MASK_OFFSET);
+	/* unmask necessary irqs */
+	uart_bflb_irq_rx_enable(dev);
+	uart_bflb_irq_err_enable(dev);
+	/* enable all irqs */
+	sys_write32(0xFF, cfg->base_reg + UART_INT_EN_OFFSET);
+	cfg->irq_config_func(dev);
+#endif /* CONFIG_UART_INTERRUPT_DRIVEN */
+	uart_bflb_enabled(dev, 1);
+	return rc;
 }
 
 static int ITCMF uart_bflb_poll_in(const struct device *dev, unsigned char *c)
@@ -314,16 +531,18 @@ static void ITCMF uart_bflb_poll_out(const struct device *dev, unsigned char c)
 	while ((sys_read32(cfg->base_reg + UART_FIFO_CONFIG_1_OFFSET) & UART_TX_FIFO_CNT_MASK) == 0)
 	{
 		/* TODO: Incorrect usage of mtime */
-		if ((sys_clock_cycle_get_64() - start_time) > 100000) {
+		/*if ((sys_clock_cycle_get_64() - start_time) > 10000000) {
 			return;
-		}
+		}*/
 	}
 	sys_write8(c, cfg->base_reg + UART_FIFO_WDATA_OFFSET);
 	return;
 }
 
+
+
 #ifdef CONFIG_PM_DEVICE
-static int uart_bl_pm_control(const struct device *dev,
+static int uart_bflb_pm_control(const struct device *dev,
 			      enum pm_device_action action)
 {
 	const struct bl_config *cfg = dev->config;
@@ -368,29 +587,73 @@ static int uart_bl_pm_control(const struct device *dev,
 static const struct uart_driver_api uart_bflb_driver_api = {
 	.poll_in = uart_bflb_poll_in,
 	.poll_out = uart_bflb_poll_out,
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	.err_check = uart_bflb_err_check,
+	.fifo_fill = uart_bflb_fifo_fill,
+	.fifo_read = uart_bflb_fifo_read,
+	.irq_tx_enable = uart_bflb_irq_tx_enable,
+	.irq_tx_disable = uart_bflb_irq_tx_disable,
+	.irq_tx_ready = uart_bflb_irq_tx_ready,
+	.irq_tx_complete = uart_bflb_irq_tx_complete,
+	.irq_rx_enable = uart_bflb_irq_rx_enable,
+	.irq_rx_disable = uart_bflb_irq_rx_disable,
+	.irq_rx_ready = uart_bflb_irq_rx_ready,
+	.irq_err_enable = uart_bflb_irq_err_enable,
+	.irq_err_disable = uart_bflb_irq_err_disable,
+	.irq_is_pending = uart_bflb_irq_is_pending,
+	.irq_update = uart_bflb_irq_update,
+	.irq_callback_set = uart_bflb_irq_callback_set,
+#endif /* CONFIG_UART_INTERRUPT_DRIVEN */
 };
+
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+#define BFLB_UART_IRQ_HANDLER_DECL(instance)						\
+	static void ITCMF uart_bflb_config_func_##instance(const struct device *dev);
+#define BFLB_UART_IRQ_HANDLER_FUNC(instance)						\
+	.irq_config_func = uart_bflb_config_func_##instance
+#define BFLB_UART_IRQ_HANDLER(instance)							\
+	static void ITCMF uart_bflb_config_func_##instance(const struct device *dev)	\
+	{										\
+		IRQ_CONNECT(DT_INST_IRQN(instance),					\
+			    DT_INST_IRQ(instance, priority),				\
+			    uart_bflb_isr,						\
+			    DEVICE_DT_INST_GET(instance),				\
+			    0);								\
+		irq_enable(DT_INST_IRQN(instance));					\
+	}
+#else /* CONFIG_UART_INTERRUPT_DRIVEN */
+#define BFLB_UART_IRQ_HANDLER_DECL(instance)
+#define BFLB_UART_IRQ_HANDLER_FUNC(instance)
+#define BFLB_UART_IRQ_HANDLER(instance)
+#endif /* CONFIG_UART_INTERRUPT_DRIVEN */
+
 
 #define BL_UART_INIT(instance)							\
 	PINCTRL_DT_INST_DEFINE(instance);					\
-	static const struct bflb_config bl_uart##instance##_config = {		\
+	BFLB_UART_IRQ_HANDLER_DECL(instance)					\
+	static struct bflb_data uart##instance##_bflb_data;			\
+	static const struct bflb_config uart##instance##_bflb_config = {	\
 		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(instance),		\
 		.base_reg = DT_INST_REG_ADDR(instance),				\
 										\
-		.baudrate = DT_INST_PROP(instance, current_speed),	\
-		.data_bits = UART_DATA_BITS_8,				\
-		.stop_bits = UART_STOP_BITS_1,				\
-		.parity = UART_PARITY_NONE,				\
-		.bit_order = UART_MSB_FIRST,				\
-		.flow_ctrl = UART_FLOWCTRL_NONE,			\
-		.tx_fifo_threshold = 15,					\
-		.rx_fifo_threshold = 15,					\
-										\
+		.baudrate = DT_INST_PROP(instance, current_speed),		\
+		.data_bits = UART_DATA_BITS_8,					\
+		.stop_bits = UART_STOP_BITS_1,					\
+		.parity = UART_PARITY_NONE,					\
+		.bit_order = UART_MSB_FIRST,					\
+		.flow_ctrl = UART_FLOWCTRL_NONE,				\
+		/* overflow interupt thresold, size is 32 bytes*/		\
+		.tx_fifo_threshold = 8,						\
+		.rx_fifo_threshold = 1,						\
+		BFLB_UART_IRQ_HANDLER_FUNC(instance)				\
 	};									\
 	DEVICE_DT_INST_DEFINE(instance, &uart_bflb_init,			\
-			      uart_bl_pm_control,				\
-			      NULL,						\
-			      &bl_uart##instance##_config, PRE_KERNEL_1,	\
+			      uart_bflb_pm_control,				\
+			      &uart##instance##_bflb_data,			\
+			      &uart##instance##_bflb_config, PRE_KERNEL_1,	\
 			      CONFIG_KERNEL_INIT_PRIORITY_DEVICE,		\
-			      &uart_bflb_driver_api);
+			      &uart_bflb_driver_api);				\
+										\
+	BFLB_UART_IRQ_HANDLER(instance)
 
 DT_INST_FOREACH_STATUS_OKAY(BL_UART_INIT)
